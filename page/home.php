5f82a9b39f406ed7694c53405b2a229b2bf6fe28
Swati-Agrawal <swati97agrawal@gmail.com>
2016-04-09 01:29:08 +0530

    first commit

---
+++ page/home.php
@@ -0,0 +1,44 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>HOME</title>
+</head>
+<body link="#ffffff" bgcolor="#C8E4FF" alink="Black" vlink="black">
+<table border=0 width="100%" align="center">
+ <tr bgcolor="#C8E4FF">
+  <td align="center">
+    <center><table border=0 width=75%>
+       <tr bgcolor="#C8E4FF" align="center">
+          <td align="left"><img src="./images/logo1.png" width="280" height="150" ></td>
+         <td align="Right"><br><br><br><br><br><font face="Calibri" size="5"><a href="admi.php">Admin Login</a></font></td>
+       </tr>
+       <tr bgcolor="teal" >
+          <td height="40" colspan="3"><pre><font face="Arial" size="4"> <a href="home.php">Home</a>   |   <a href="abo.php">About Us</a>   |   <a href="lery.php">Photo Gallery</a>   |   <a href="brnch.php">Branch</a>   |   <a href="p.php">Help</a></pre></font></td>
+       </tr>
+
+       <tr bgcolor="#ffffff">
+<td colspan="3"><img src="./images/h.png" width="1000" height="250" ></td>
+       </tr>
+<tr>
+<td colspan=3><a href="v.php"><img src="./images/view.png" width="1000" height="250" ></a></td>
+</tr>
+<tr>
+<td colspan=3><a href="search.php"><img src="./images/view2.png" width="1000" height="270" ></a></td>
+</tr>
+          
+                   
+       <tr bgcolor="teal">
+          <td height="40" colspan="3" align="left"><pre><font face="Arial" size="4"> <a href="home.php">Home</a> | <a href="about.php">About Us</a> | <a href="gallery.php">Photo Gallery</a> | <a href="branch.php">Branch</a> | <a href="p.php">Help</a></pre></font></td> 
+          </td>
+       </tr>
+      <tr bgcolor="#F89B51">
+          <td align="center" colspan="3"><font face="Arial" size="2" color="#000000">&copy; Copyright Digital Library 2014-2015. All rights reserved.</font><br>
+              Made By: Deepti Garg And Swati Agrawal-  <a href=c.php>Contact Us</a></td>
+      </tr> 
+    </table>
+  </td>
+ </tr>
+</table>
+</body>
+</html>
+
